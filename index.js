e4ef3acac9746966064965f8bec2001d4f10659e
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-10-24 22:37:05 +0300

    Update CORS origin array to include new production URL

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const USER_NAME = process.env.USER_NAME
 const PASSWORD = process.env.PASSWORD
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://abdalla-elhagar.github.io"],
+  origin: ["http://localhost:5173", "https://abdalla-elhagar.github.io","https://re-book-0.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
